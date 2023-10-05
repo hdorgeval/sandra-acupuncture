268d8056a97eb62c58d9def2ff601673c25606c3
Henri d'Orgeval <henri.dorgeval@outlook.fr>
2023-10-05 13:42:23 +0200

    feat(massage-couple): add rendez-vous

--- src/pages/MassageCouple.tsx
+++ src/pages/MassageCouple.tsx
@@ -1,9 +1,11 @@
 import { FC } from 'react';
 import { CallablePhoneNumberButton } from '../components/CallablePhoneNumberButton';
 import { OpenExternalLinkButton } from '../components/OpenExternalLinkButton';
+import { OpenModalButton } from '../components/OpenModalButton';
 import { PhoneNumber } from '../components/PhoneNumber';
 import { Quote } from '../components/Quote';
 import { ReadMoreReadLess } from '../components/ReadMoreReadLess';
+import { RendezVousModal } from '../components/RendezVousModal';
 import { websiteConfig } from '../website.config';
 import { PageTitle } from './page-layout/PageTitle';
 import { PublicPageLayoutWithFixedBackgroundImage } from './page-layout/PublicPageLayoutWithFixedBackgroundImage';
@@ -22,6 +24,12 @@ export const MassageCouple: FC = () => {
         <div className="d-flex flex-column justify-content-start align-items-center">
           <div className="mt-7">
             <PageTitle>Massage Couple</PageTitle>
+            <OpenModalButton
+              className="btn btn-outline-light fw-bolder w-100 mt-1"
+              modalId="prendre-rendez-vous"
+            >
+              Prendre rendez-vous
+            </OpenModalButton>
           </div>
           <div className="" style={{ minHeight: '5vh' }}></div>
           <div className="container-fluid d-flex flex-row justify-content-start align-items-stretch flex-wrap py-2 px-0">
@@ -43,9 +51,17 @@ export const MassageCouple: FC = () => {
                     À travers cette expérience partagée vous renforcerez les liens de coeur et d'âme
                     de votre couple.
                   </p>
-                  <p className="mb-0 pb-0">
+                  <p className="">
                     Transportés par le toucher doux et sensuel de nos mains, la connexion avec votre
                     partenaire vous fera voyager dans un espace divin.
+                  </p>
+                  <p className="fw-bolder mt-2 mb-0 pb-0">
+                    <OpenModalButton
+                      className="btn btn-outline-light fw-bolder mt-0 mb-1"
+                      modalId="prendre-rendez-vous"
+                    >
+                      Prendre rendez-vous
+                    </OpenModalButton>
                   </p>
                 </ReadMoreReadLess>
               </SemiTransparentTile>
@@ -135,18 +151,13 @@ export const MassageCouple: FC = () => {
                     <i className="bi bi-calendar-event fs-2 me-4"></i>
                     <div className="d-flex flex-column card-subtitle">
                       <span className="">Rdv validé uniquement après entretien téléphonique</span>
-                      <OpenExternalLinkButton
+                      <OpenModalButton
                         className="btn btn-outline-light fw-bolder w-100 mt-1"
-                        link={websiteConfig.links.rdv.url}
-                        title="Prendre rendez-vous pour l'entretien téléphonique"
-                        analyticsEvent="rdv-massage-eveil-des-sens"
+                        modalId="prendre-rendez-vous"
                       >
-                        <div className="d-flex flex-column align-items-center">
-                          <span className="">
-                            Prendre rendez-vous pour l'entretien téléphonique
-                          </span>
-                        </div>
-                      </OpenExternalLinkButton>
+                        Prendre rendez-vous
+                      </OpenModalButton>
+
                       <span className="mt-4">Ou bien appelez-moi directement :</span>
                       <CallablePhoneNumberButton
                         className="btn btn-outline-light fw-bolder w-100 mt-1"
@@ -205,12 +216,30 @@ export const MassageCouple: FC = () => {
                         Le massage sera suivi d'un temps d'intégration pour revenir à vous en
                         douceur.
                       </p>
+                      <p>
+                        <OpenModalButton
+                          className="btn btn-outline-light fw-bolder w-100 mt-1"
+                          modalId="prendre-rendez-vous"
+                        >
+                          Prendre rendez-vous
+                        </OpenModalButton>
+                      </p>
                     </div>
                   </div>
                 </TransparentListGroupItem>
               </ul>
             </SemiTransparentTile>
           </div>
+
+          <RendezVousModal
+            modalId="prendre-rendez-vous"
+            analyticsEvent="rdv-form-massage-couple"
+            subjectOptions={[
+              { label: 'Prendre un rendez vous Massage Couple' },
+              { label: 'Autre' },
+            ]}
+            backgroundColor="#2c1b91"
+          ></RendezVousModal>
         </div>
       </PublicPageLayoutWithFixedBackgroundImage>
     </>
